3e7c7b4c421bbec49091ad8c5b0b0a310df0e5ea
fang <787503044@qq.com>
2017-08-05 12:07:56 +0800

    sd
    sd

---
+++ com/neu/src/bean/Paper.java
@@ -0,0 +1,132 @@
+package bean;
+
+public class Paper {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public java.sql.Date getStarttime() {
+		return starttime;
+	}
+	public void setStarttime(java.sql.Date starttime) {
+		this.starttime = starttime;
+	}
+	public java.sql.Date getEndtime() {
+		return endtime;
+	}
+	public void setEndtime(java.sql.Date endtime) {
+		this.endtime = endtime;
+	}
+	public String getPapername() {
+		return papername;
+	}
+	public void setPapername(String papername) {
+		this.papername = papername;
+	}
+	public int getXzt1() {
+		return xzt1;
+	}
+	public void setXzt1(int xzt1) {
+		this.xzt1 = xzt1;
+	}
+	public int getXzt2() {
+		return xzt2;
+	}
+	public void setXzt2(int xzt2) {
+		this.xzt2 = xzt2;
+	}
+	public int getXzt3() {
+		return xzt3;
+	}
+	public void setXzt3(int xzt3) {
+		this.xzt3 = xzt3;
+	}
+	public int getXzt4() {
+		return xzt4;
+	}
+	public void setXzt4(int xzt4) {
+		this.xzt4 = xzt4;
+	}
+	public int getXzt5() {
+		return xzt5;
+	}
+	public void setXzt5(int xzt5) {
+		this.xzt5 = xzt5;
+	}
+	public int getXzt6() {
+		return xzt6;
+	}
+	public void setXzt6(int xzt6) {
+		this.xzt6 = xzt6;
+	}
+	public int getTkt1() {
+		return tkt1;
+	}
+	public void setTkt1(int tkt1) {
+		this.tkt1 = tkt1;
+	}
+	public int getTkt2() {
+		return tkt2;
+	}
+	public void setTkt2(int tkt2) {
+		this.tkt2 = tkt2;
+	}
+	public int getTkt3() {
+		return tkt3;
+	}
+	public void setTkt3(int tkt3) {
+		this.tkt3 = tkt3;
+	}
+	public int getTkt4() {
+		return tkt4;
+	}
+	public void setTkt4(int tkt4) {
+		this.tkt4 = tkt4;
+	}
+	public int getTkt5() {
+		return tkt5;
+	}
+	public void setTkt5(int tkt5) {
+		this.tkt5 = tkt5;
+	}
+	public int getTkt6() {
+		return tkt6;
+	}
+	public void setTkt6(int tkt6) {
+		this.tkt6 = tkt6;
+	}
+	public int getBct1() {
+		return bct1;
+	}
+	public void setBct1(int bct1) {
+		this.bct1 = bct1;
+	}
+	public int getBct2() {
+		return bct2;
+	}
+	public void setBct2(int bct2) {
+		this.bct2 = bct2;
+	}
+	private int id;
+	private java.sql.Date starttime;
+	private java.sql.Date endtime;
+	private String papername;
+	private int xzt1;
+	private int xzt2;
+	private int xzt3;
+	private int xzt4;
+	private int xzt5;
+	private int xzt6;
+	private int tkt1;
+	private int tkt2;
+	private int tkt3;
+	private int tkt4;
+	private int tkt5;
+	private int tkt6;
+	private int bct1;
+	private int bct2;
+
+	
+}
---
+++ com/neu/src/bean/Student.java
@@ -0,0 +1,69 @@
+package bean;
+
+public class Student {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	public String getStudentclass() {
+		return studentclass;
+	}
+	public void setStudentclass(String studentclass) {
+		this.studentclass = studentclass;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getRu() {
+		return ru;
+	}
+	public void setRu(int ru) {
+		this.ru = ru;
+	}
+	private int id;
+	private String username;
+	private String pwd;
+	private String name;
+	private char sex;
+	private String studentclass;
+	private String telephone;
+	private String email;
+	private int ru;
+	
+
+}
